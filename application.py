cb2e579c4abbb62b7bf1cbce16275b67b07f7def
Apoorv Kulshreshtha <apoorvkuls@gmail.com>
2016-10-27 21:42:52 -0400

    Final Code

--- application.py
+++ application.py
@@ -6,47 +6,45 @@ application = Flask(__name__)
 chab = ""
 
 @application.route("/")
-def home():
-    es = Elasticsearch([{'host':'search-tweet-c3mkhiza33cstn5qrvotrsw7my.us-west-2.es.amazonaws.com', 'port':443,'use_ssl':True}])
-    result = es.search(size=5000,index='twitter')
-    return render_template('tweet.html', result=parseRes(result))
+def index():
+    elsr = Elasticsearch([{'host':'search-twitter-es-mdmyclrn5jzxcopqx4houjyoo4.us-east-1.es.amazonaws.com', 'port':443,'use_ssl':True}])
+    result = elsr.search(size=5000,index='twitter')
+    return render_template('tweet.html', result=parse(result))
 
-@application.route('/keysearch', methods = ['GET','POST'])
-def keysearch():
-    keywords = request.form['search']
-    es = Elasticsearch([{'host':'search-tweet-c3mkhiza33cstn5qrvotrsw7my.us-west-2.es.amazonaws.com', 'port':443,'use_ssl':True}])
-    updateKeywords(str(keywords))
-    result = tweetmatch(es, str(keywords))
-    return render_template('tweet.html', result=parseRes(result))
+@application.route('/ks', methods = ['GET','POST'])
+def ks():
+    key = request.form['search']
+    elsr = Elasticsearch([{'host':'search-twitter-es-mdmyclrn5jzxcopqx4houjyoo4.us-east-1.es.amazonaws.com', 'port':443,'use_ssl':True}])
+    updateKeywords(str(key))
+    result = tweetmatch(elsr, str(key))
+    return render_template('tweet.html', result=parse(result))
 
-def tweetmatch(es, keyword):
-    if len(keyword) is not 0:
-      res = es.search(size=5000, index="twitter", body={"query": {"query_string": {"query": keyword}}})
+def tweetmatch(elsr, key):
+    if len(key) is not 0:
+      r = elsr.search(size=5000, index="twitter", body={"query": {"query_string": {"query": key}}})
     else:
-      res = es.search(size=5000,index='twitter')
-    return res
+      r = elsr.search(size=5000,index='twitter')
+    return r
 
-def updateKeywords(keywordList):
+def updateKeywords(keyList):
   global chab
-  chab = keywordList
+  chab = keyList
 
-def parseRes(result):
-  for r in result['hits']['hits']:
-    r['_source']['text'] = ''.join(i for i in r['_source']['text'] if ord(i)<128)
-  return result
+def parse(resul):
+  for rl in resul['hits']['hits']:
+    rl['_source']['text'] = ''.join(i for i in rl['_source']['text'] if ord(i)<128)
+  return resul
 
-@application.route('/rt', methods = ['GET','POST'])
-def rt():
-  es = Elasticsearch([{'host':'search-tweet-c3mkhiza33cstn5qrvotrsw7my.us-west-2.es.amazonaws.com', 'port':443,'use_ssl':True}])
+@application.route('/rtw', methods = ['GET','POST'])
+def rtw():
+  elsr = Elasticsearch([{'host':'search-twitter-es-mdmyclrn5jzxcopqx4houjyoo4.us-east-1.es.amazonaws.com', 'port':443,'use_ssl':True}])
   global chab
   if len(chab) is not 0:
-    result = es.search(size=5000, index="twitter", body={"query": {"query_string": {"query": chab}}})
+    result = elsr.search(size=5000, index="twitter", body={"query": {"query_string": {"query": chab}}})
   else:
-    result = es.search(size=5000,index='twitter')
-  print('server loop')
-  print(len(result['hits']['hits']))
-  print('exit server loop')
-  return jsonify(parseRes(result))
+    result = elsr.search(size=5000,index='twitter')
+
+  return jsonify(parse(result))
 
   
 if __name__ == "__main__":
